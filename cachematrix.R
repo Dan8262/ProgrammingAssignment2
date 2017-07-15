9d1b6ff560cabf480375eeec2e8259f0349277fb
Daniel Caussé <daniel.causse@aviation-militaire.com>
2017-07-15 18:44:43 +0200

    Motif cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Mise en cache de la matrice inverse d'une matrice x
+##  passée en paramètre.
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
         set <- function(y) {
@@ -20,14 +21,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## Fonction permettant d'obtenir à partir du cache
+##  la matrice inverse d'une matrice fournie en paramètre.
+## Si la matrice inverse n'est pas définie dans le cache, on la calcule.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverse <- x$getinverse()
         if(!is.null(inverse)) {
+                ## Récupération de la matrice inverse dans le cache.
                 message("getting cached data")
                 return(inverse)
         }
+        ## Matrice inverse absente du cache, on la calcule
         data <- x$get()
         inverse <- solve(data)
         x$setinverse(inverse)
